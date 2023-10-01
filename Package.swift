0d9767596ddeaa86180aa98da4d75e28d3d07964
Christoph Hagen <github@christophhagen.de>
2023-10-01 23:51:27 +0200

    Use new scheduler

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["ClairvoyantLogging"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/christophhagen/Clairvoyant", from: "0.9.2"),
+        .package(url: "https://github.com/christophhagen/Clairvoyant", from: "0.11.2"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.5.0"),
 
     ],
